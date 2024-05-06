68c6f79a3db00664a738a10a487e30ab1f4607be
v.bazhenov <sleem81@list.ru>
2024-05-06 18:06:05 +0500

    Перенес методы из UserServis в UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,13 +8,13 @@ public class Util {
     private static final String DB_URL = "jdbc:mysql://localhost/maven";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "";
-    public Connection getConnection () {
+    public static Connection getConnection () {
         Connection connection = null;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            System.out.println("Connection to User DB succesfull!");
+           // System.out.println("Connection to User DB succesfull!");
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Connection ERROR!");
@@ -22,22 +22,3 @@ public class Util {
         return connection;
     }
 }
-//    public static void main(String[] args) {
-//        final String DB_URL = "jdbc:mysql://localhost/maven";
-//        final String DB_USERNAME = "root";
-//        final String DB_PASSWORD = "";
-//
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-//            try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
-//
-//                System.out.println("Connection to Store DB succesfull!");
-//            }
-//        } catch (Exception ex) {
-//            System.out.println("Connection failed...");
-//
-//            System.out.println(ex);
-//
-//        }
-//    }
-//}
